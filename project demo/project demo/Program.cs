a02cd1e8adb5cc66c894f7f8e56feab3484a2249
krunal310 <krunalvasoya@gmail.com>
2020-01-11 14:46:01 +0530

    Password Asterisk Done

--- project demo/project demo/Program.cs
+++ project demo/project demo/Program.cs
@@ -12,7 +12,8 @@ namespace project_demo
             Console.WriteLine("Enter User ID: ");
             int id = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter Password: ");
-            string pass = Console.ReadLine();
+            string pass = PassAsterisk();
+            Console.ReadLine();
             admin o = new admin();
             int i = o.login(id, pass);
 
@@ -28,6 +29,39 @@ namespace project_demo
                 Console.WriteLine("Failed");
 
             }
+        }
+
+        public static string PassAsterisk()
+        {
+            string pass = "";
+            ConsoleKeyInfo info = Console.ReadKey(true);
+            while (info.Key != ConsoleKey.Enter)
+            {
+                if (info.Key != ConsoleKey.Backspace)
+                {
+                    Console.Write("*");
+                    pass += info.KeyChar;
+                }
+                else if (info.Key == ConsoleKey.Backspace)
+                {
+                    if (!string.IsNullOrEmpty(pass))
+                    {
+                        // remove one character from the list of password characters
+                        pass = pass.Substring(0, pass.Length - 1);
+                        // get the location of the cursor
+                        int pos = Console.CursorLeft;
+                        // move the cursor to the left by one character
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                        // replace it with space
+                        Console.Write(" ");
+                        // move the cursor to the left by one character again
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                    }
+                }
+                info = Console.ReadKey(true);
+            }
+            // add a new line because user pressed enter at the end of their password
+            return pass;
         }
     }
 }
